b168c527305e58fe5be6ad85d37bd2d6490c8d81
liadk07 <91607695+liad07@users.noreply.github.com>
2022-05-29 03:49:02 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screenshot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            System.Threading.Thread.Sleep(1000);
+            SendKeys.Send("{PRTSC}");
+            Image img = Clipboard.GetImage();
+            pictureBox1.Image = img;
+            img.Save("E:\\" + textBox1.Text+".jpg");
+            this.Show();
+
+        }
+    }
+}
